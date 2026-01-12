54218f4ec9707a00e517a035a7ccc509206cf558
Sidiney Matheus <z3theus@icloud.com>
2026-01-12 18:04:02 +0000

    Atualizando endpoint de status para tratar erros de conexão

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  // Adicionado async
+  const result = await database.query("SELECT 1 + 1 AS sum;"); // Corrigido AS
+  console.log(result.rows); // Corrigido rows
+
   response.status(200).json({ chave: "sao acima da media" });
 }
 
