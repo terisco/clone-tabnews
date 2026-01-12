2782e68d82e56af87075f0f109f677ddc142ad55
Sidiney Matheus <z3theus@icloud.com>
2026-01-12 18:03:43 +0000

    Corrigindo conexão e variáveis do banco de dados

---
+++ infra/database.js
@@ -0,0 +1,26 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB, // Ajustado para bater com o .env
+  });
+
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error("Erro na query:", error);
+    throw error;
+  } finally {
+    await client.end();
+  }
+}
+
+export default {
+  query: query,
+};
